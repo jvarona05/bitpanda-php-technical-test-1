28a3c45cffcbba8d937ac87b371e6295ca41d346
José <jose.varona05@gmail.com>
2020-04-16 07:10:49 +0200

    get user tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,10 +10,39 @@ use Tests\Utils\UserUtils;
 class UserTest extends ApiTestCase
 {
     /** @test */
+    public function get_active_users_from_russia()
+    {        
+        $filters = ['active' => 1, 'country' => 'RU'];
+        
+        $this->getUsers($filters)->assertJsonCount(1, 'data'); 
+    }
+
+    /** @test */
+    public function get_active_users()
+    {        
+        $filters = ['active' => 1];
+        
+        $this->getUsers($filters)->assertJsonCount(5, 'data'); 
+    }
+
+    /** @test */
+    public function get_all_unactive_users()
+    {       
+        $filters = ['active' => 0];
+
+        $this->getUsers($filters)->assertJsonCount(2, 'data'); 
+    }
+
+    /** @test */
     public function get_all_users()
     {        
-        $this->getJson(route('api.users'))
-            ->assertStatus(200)
-            ->assertJsonStructure(UserUtils::$usersStructure);
+        $this->getUsers()->assertJsonCount(7, 'data');
+    }
+
+    private function getUsers(array $filters = [])
+    {
+        return $this->getJson(route('api.users', $filters))
+                    ->assertStatus(200)
+                    ->assertJsonStructure(UserUtils::$usersStructure);
     }
 }
