91d88cfa140ea81b917c6287c794c31eb53f2512
José <jose.varona05@gmail.com>
2020-04-16 07:43:22 +0200

    add new tests
    -get_active_users_from_russia
    -get_an_error_if_the_country_is_not_in_the_db
    -get_an_error_if_an_invalid_active_value_is_sent

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -10,11 +10,9 @@ use Tests\Utils\UserUtils;
 class UserTest extends ApiTestCase
 {
     /** @test */
-    public function get_active_users_from_russia()
+    public function get_all_users()
     {        
-        $filters = ['active' => 1, 'country' => 'RU'];
-        
-        $this->getUsers($filters)->assertJsonCount(1, 'data'); 
+        $this->getUsers()->assertJsonCount(7, 'data');
     }
 
     /** @test */
@@ -26,7 +24,7 @@ class UserTest extends ApiTestCase
     }
 
     /** @test */
-    public function get_all_unactive_users()
+    public function get_inactive_users()
     {       
         $filters = ['active' => 0];
 
@@ -34,9 +32,47 @@ class UserTest extends ApiTestCase
     }
 
     /** @test */
-    public function get_all_users()
+    public function get_all_users_from_austria()
     {        
-        $this->getUsers()->assertJsonCount(7, 'data');
+        $filters = ['country' => 'AT'];
+        
+        $this->getUsers($filters)->assertJsonCount(3, 'data'); 
+    }
+
+    /** @test */
+    public function get_active_users_from_russia()
+    {        
+        $filters = ['active' => 1, 'country' => 'RU'];
+        
+        $this->getUsers($filters)->assertJsonCount(1, 'data'); 
+    }
+
+    /** @test */
+    public function get_an_error_if_the_country_is_not_in_the_db()
+    {        
+        $filters = ['country' => 'DO'];
+        
+        $this->getJson(route('api.users', $filters))
+                    ->assertStatus(422)
+                    ->assertJsonFragment([
+                        'errors' => [
+                            'country' => ['The selected country is invalid.']
+                        ]
+                    ]);
+    }
+
+    /** @test */
+    public function get_an_error_if_an_invalid_active_value_is_sent()
+    {        
+        $filters = ['active' => 'this is a bad value'];
+        
+        $this->getJson(route('api.users', $filters))
+                    ->assertStatus(422)
+                    ->assertJsonFragment([
+                        'errors' => [
+                            'active' => ['The active field must be true or false.']
+                        ]
+                    ]);
     }
 
     private function getUsers(array $filters = [])
